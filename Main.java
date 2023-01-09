513c6299b619314bcac0b90180a8c939cee46be4
shristisaxena <117073006+shristisaxena@users.noreply.github.com>
2023-01-09 18:45:36 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.gl.main;
+
+import java.util.Scanner;
+import com.gl.driver.MergeSort;
+import com.gl.driver.NotesCount;
+
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		MergeSort mergeSort = new MergeSort();
+		NotesCount notesCount = new NotesCount();
+		
+		System.out.println("Enter the size of currency denominations: ");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int[] notes = new int[size];
+		
+		System.out.println("Enter the denominations");
+		
+		for (int i = 0; i < size; i++) {
+			notes[i] = sc.nextInt();
+		}
+		
+		System.out.println("Enter the amount you want to pay: ");
+		int amount = sc.nextInt();
+		mergeSort.sort(notes,  0, notes.length-1);
+		notesCount.notesCountImplementation(notes, amount);
+	}
+
+}
